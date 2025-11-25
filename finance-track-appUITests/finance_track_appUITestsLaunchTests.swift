4b9ba871033560ffe9c461d8c4e99962efafc8b4
Matheus de Sousa Matos <69324938+Matheus-Sousa-Matos@users.noreply.github.com>
2025-11-25 14:42:00 -0300

    Initial Commit

---
+++ finance-track-appUITests/finance_track_appUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  finance_track_appUITestsLaunchTests.swift
+//  finance-track-appUITests
+//
+//  Created by Matheus de Sousa Matos on 25/11/25.
+//
+
+import XCTest
+
+final class finance_track_appUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
